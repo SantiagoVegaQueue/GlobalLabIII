11806c5be3dfba465c91bdfc03b0748cac5f2e1f
Santiago Vega <josevegasantiago8919@gmail.com>
2020-05-31 19:16:35 -0300

    Agrege el form en el login y agregue un php para el panel del admin

--- login.php
+++ login.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LOGIN</title>
 </head>
 <body>
+    <h1>LOG IN</h1>
     <form action="">
-        <input type="text" placeholder="2asdasd">
+        <input type="text" id="login" placeholder="Usuario"><br>
+        <br><input type="password" id="password" placeholder="Contraseña"><br>
+        <br><input type="submit" id="iniciar_sesion" value="Iniciar sesión">
     </form>
 </body>
 </html>
---
+++ panel_admin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PANEL ADMIN</title>
+</head>
+<body>
+<h1>PANEL ADMIN</h1>
+    <form action="">
+        <h2>Subir productos</h2><br>
+        <input type="file" id="subir_archivo" multiple><br>
+        <br><label for="">Descripcion del producto</label><br>
+        <textarea name="descripcion" id="descripcion" cols="50" rows="20" placeholder="Añade una breve descripción del producto"></textarea><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
